e021070886e4ae583c1ff7540b24a17ed5f4a270
Ilya <ilirhin@gmail.com>
2015-05-19 15:00:19 +0300

    Utils modified

--- utils.h
+++ utils.h
@@ -7,6 +7,10 @@
 #ifndef SHAD_CPLUSPLUS_PROJECT_UTILS_H
 #define SHAD_CPLUSPLUS_PROJECT_UTILS_H
 
+double getNorm(const Point &vec) {
+    return sqrt(vec.x_ * vec.x_ + vec.y_ * vec.y_);
+}
+
 double dist(const Point &first, const Point &second) {
     return sqrt((first.x_ - second.x_) * (first.x_ - second.x_) +
                 (first.y_ - second.y_) * (first.y_ - second.y_));
@@ -17,12 +21,16 @@ double product(const Point &first, const Point &second) {
 }
 
 double rotateCos(const Point &first, const Point &second, const Point &third) {
-    double fx = first.x_;
-    double sx = second.x_;
-    double fy = first.y_;
-    double sy = second.y_;
+    double fx = second.x_ - first.x_;
+    double sx = third.x_ - second.x_;
+    double fy = second.y_ - first.y_;
+    double sy = third.y_ - second.y_;
 
     return (fx * sx + fy * sy) / sqrt(fx * fx + fy * fy) / sqrt(sx * sx + sy * sy);
+}
+
+double cosBetweenVectors(const Point &first, const Point &second) {
+    return product(first, second) / getNorm(first) / getNorm(second);
 }
 
 #endif //SHAD_CPLUSPLUS_PROJECT_UTILS_H
